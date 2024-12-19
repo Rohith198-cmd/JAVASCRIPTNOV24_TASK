834e94dfb50c95dbbfb591463146021c2165ec04
Rohith198-cmd <rohithpalani67@gmail.com>
2024-12-19 20:13:57 +0530

    calculator completed

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("helloworld")
+const display=document.getElementById('display')
+
+function append(value){
+    display.value+=value
+}
+function cleardisplay(){
+    display.value=''
+}
+function calculate(){
+    display.value=eval(display.value);
+}
